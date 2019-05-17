83f370ae37763e03f9cf300e0efb45ada1fc01a0
Beni Cherniavsky-Paskin <cben@redhat.com>
2019-05-17 13:37:46 +0300

    play animations, interactive REPL, WIP Hebrew
    HACK: forced RTL by starting some lines with 'ע'.
    To get RTL hebrew you need a bidi terminal like mlterm or,
    amusingly, pterm (Putty).

--- lang.js
+++ lang.js
@@ -64,6 +64,8 @@ const fixedColor = (red, green, blue) => ({
 
 words.black = constWord(fixedColor(0, 0, 0))
 words.red = constWord(fixedColor(COLOR_MAX, 0, 0))
+words.green = constWord(fixedColor(0, COLOR_MAX, 0))
+words.blue = constWord(fixedColor(0, 0, COLOR_MAX))
 words.white = constWord(fixedColor(COLOR_MAX, COLOR_MAX, COLOR_MAX))
 
 const finalColor = anim => anim.color(anim.duration)
@@ -86,6 +88,20 @@ words.reverse = unaryWord(a => (
   }
 ))
 
+words.slow = unaryWord(a => (
+  {
+    duration: a.duration * 2,
+    color: time => a.color(time / 2),
+  }
+))
+
+words.fast = unaryWord(a => (
+  {
+    duration: a.duration / 2,
+    color: time => a.color(time * 2),
+  }
+))
+
 const binaryWord = binaryFunc => (
   stack => {
     const [x, y, ...rest] = stack
@@ -105,45 +121,121 @@ words.fade = binaryWord((a1, a2) => {
 })
 
 // TODO naming: definitely not "concat". maybe "append"?
-words.join = binaryWord((a1, a2) => (
+words.join = binaryWord((a2, a1) => (
   {
     duration: a1.duration + a2.duration,
     color: time => time < a1.duration ? a1.color(time) : a2.color(time - a1.duration),
   }
 ))
 
+// 2 -> 2 word
+
+words.swap = ([x, y, ...rest]) => [y, x, ...rest]
+
+// i18n
+
+hebrewWords = {
+  שחור: words.black,
+  ירוק: words.green,
+  אדום: words.red,
+  כחול: words.blue,
+  לבן: words.white,
+  הפוך: words.reverse, הפוכ: words.reverse,
+  מהר: words.fast,
+  לאט: words.slow,
+  מעבר: words.fade,
+  חבר: words.join,
+  החלף: words.swap,
+}
+
 // REPL
 // ----
 
+const readline = require('readline')
 const chalk = require('chalk')
 
-const showColor = color => chalk.rgb(Math.round(color.red), Math.round(color.green), Math.round(color.blue))('█')
+const sleep = milliSeconds => new Promise(resolve => setTimeout(resolve, milliSeconds))
+
+const showColor = color => chalk.rgb(Math.round(color.red), Math.round(color.green), Math.round(color.blue))
 
 const showAnim = anim => {
   let s = ''
   for(time = 0; time <= anim.duration; time += 0.2) {
-    s += showColor(anim.color(time))
+    s += showColor(anim.color(time))('█') // U+2588 FULL BLOCK
   }
   return('[' + s + ']')
 }
 
-const showStack = stack => {
-  console.log('stack:', stack.map(showAnim).join('  '))
+const playAnim = async anim => {
+  const step = 0.05
+  for(time = 0; time <= anim.duration; time += step) {
+    const pos = Math.round(time / 0.2) // position inside [.....] above
+    const colored = showColor(anim.color(time))
+    process.stdout.write('ע' + // bidi hack
+                         ' '.repeat(3) +
+                         '(' + colored('⬤').repeat(5) + ')' + // U+2B24 BLACK LARGE CIRCLE
+                         colored('-').repeat((20 + '['.length + pos) - (3 + 1 + 5 + 1)) +
+                         '^' +
+                         '\r')
+    await sleep(step * 1000)
+  }
+  //process.stdout.write('\n')
+  process.stdout.write(' '.repeat(60) + '\r')
 }
 
-const evalWords = (stack0, program) => {
+const playStack = async stack => {
+  console.log('ע' + // bidi hack
+              ' '.repeat(20) +
+              stack.map(showAnim).join('  '))
+  if (stack.length > 0) {
+    await playAnim(stack[0])
+  }
+}
+
+// TODO extract common loop from this and interactive repl()
+const evalWords = async (dictionary, stack0, program) => {
   let stack = stack0
-  showStack(stack)
+  await playStack(stack)
+  await sleep(500)
   for(w of program) {
     console.log('#', w)
-    stack = words[w](stack)
-    showStack(stack)
+    stack = dictionary[w](stack)
+    await playStack(stack)
+    await sleep(500)
   }
   return stack
+}
+
+const repl = async (dictionary, stack0) => {
+  let stack = stack0
+  await playStack(stack)
+
+  var reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+  })
+  reader.on('line', async w => {
+    if (dictionary[w]) {
+      stack = dictionary[w](stack)
+    } else {
+      console.error('מה?')
+    }
+    await playStack(stack)
+  })
+
+  return stack // is this reachable?
 }
 
 // TEST
 // ----
 // todo move somewhere
 
-evalWords([], ['black', 'white',  'fade', 'white', 'black', 'fade', 'join'])
+const test = async () => {
+  await evalWords(words, [], ['black', 'white',  'fade', 'white', 'black', 'fade', 'swap', 'join'])
+
+  await evalWords(words, [], ['red', 'green',  'fade', 'green', 'blue', 'fade', 'slow', 'slow', 'join', 'fast'])
+}
+
+//test()
+repl(hebrewWords, [])
